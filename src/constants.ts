07700fb7305b493d34fada43dbc1374f304fd357
Kreedzt <zhaozisong1@live.com>
2022-02-05 15:13:11 +0800

    ✨  (Admin) add code paste area

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const USER_NAME_STORAGE_KEY = 'user_name';
 export const USER_ADMIN_KEY = 'admin';
 
 export const USER_ID_NOT_FOUND_MSG = '找不到用户信息';
-export const VERSION = '0.3.0';
+export const VERSION = '0.4.0';
--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useCallback, useState } from "react";
-import { Button, message } from "antd";
+import { Button, message, Input, Modal } from "antd";
 import { RouteComponentProps } from "@reach/router";
 import { code_list } from "./code_list";
 import { StashItem } from "../../models/person";
@@ -7,6 +7,7 @@ import { PersonService } from "../../services/person";
 
 const Admin: FC<RouteComponentProps> = () => {
   const [btnLoading, setBtnLoading] = useState(false);
+  const [code, setCode] = useState<string>();
 
   const updateAllPersonBackpack = useCallback(async (c: StashItem) => {
     setBtnLoading(true);
@@ -21,21 +22,80 @@ const Admin: FC<RouteComponentProps> = () => {
     setBtnLoading(false);
   }, []);
 
+  const onParseCode = useCallback(() => {
+    if (!code) {
+      message.error("代码内容为空!");
+      return;
+    }
+    try {
+      const parsedItem = JSON.parse(code) as StashItem;
+      if (
+        "key" in parsedItem &&
+        "index" in parsedItem &&
+        "class" in parsedItem
+      ) {
+        Modal.confirm({
+          title: "准备发放如下内容",
+          content: (
+            <div>
+              <p>key: {parsedItem.key}</p>
+              <p>index: {parsedItem.index}</p>
+              <p>class: {parsedItem.class}</p>
+            </div>
+          ),
+          onOk: async () => {
+            try {
+              await PersonService.insertAllPersonBackpack([parsedItem]);
+              message.success(`发放 ${parsedItem.key} 成功`);
+            } catch (e) {
+              console.log(e);
+            }
+          },
+        });
+      } else {
+        message.error("代码不合法!");
+      }
+    } catch (e) {
+      message.error("代码内容不合法!");
+      console.log("e", e);
+    }
+  }, [code]);
+
   return (
     <div className="admin">
-      <div className="quick-control-area">
-        {code_list.map((c) => {
-          const { label, ...stashInfo } = c;
-          return (
-            <Button
-              key={c.key}
-              loading={btnLoading}
-              onClick={() => updateAllPersonBackpack(stashInfo)}
-            >
-              {label}
-            </Button>
-          );
-        })}
+      <div className="global-server-command">
+        <p>全服发放</p>
+
+        <div className="quick-control-area">
+          <p>快捷操作区(快速添加一项物品)</p>
+
+          <div className="quick-btn-list">
+            {code_list.map((c) => {
+              const { label, ...stashInfo } = c;
+              return (
+                <Button
+                  key={c.key}
+                  loading={btnLoading}
+                  onClick={() => updateAllPersonBackpack(stashInfo)}
+                >
+                  {label}
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="code-paste-area">
+          <p>代码粘贴区</p>
+
+          <Input.TextArea
+            className="code-paste-input"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+            placeholder={"代码粘贴到此处"}
+          />
+          <Button onClick={onParseCode}>解析代码并添加</Button>
+        </div>
       </div>
     </div>
   );
