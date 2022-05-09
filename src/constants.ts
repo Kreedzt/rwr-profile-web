78e81c3fccd8388d8500755b86be8b6b1a3d1f97
Kreedzt <zhaozisong1@live.com>
2022-05-09 16:13:38 +0800

    :bookmark: (0.15.2)

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const USER_NAME_STORAGE_KEY = 'user_name';
 export const USER_ADMIN_KEY = 'admin';
 
 export const USER_ID_NOT_FOUND_MSG = '找不到用户信息';
-export const VERSION = '0.15.1';
+export const VERSION = '0.15.2';
 export const API_PREFIX = '/api';
