371132b9f8f92b7306cceb8c2a09e8ae6809a042
Kreedzt <zhaozisong1@live.com>
2022-09-07 20:09:24 +0800

    :bookmark: Kreedzt - release 0.20.2

--- src/constants.ts
+++ src/constants.ts
@@ -8,6 +8,6 @@ export const QUERY_LIST_STORAGE_KEY = "query_list";
 export const MAX_QUERY_LIST_STORAGE = 20;
 
 export const USER_ID_NOT_FOUND_MSG = "找不到用户信息";
-export const VERSION = "0.20.1";
+export const VERSION = "0.20.2";
 export const API_PREFIX = "/api";
 export const LOCAL_FORAGE_CONFIG_KEY = "rwr-profile-web-storage";
